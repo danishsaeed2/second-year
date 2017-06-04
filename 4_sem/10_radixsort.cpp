a9cfcf71ffa017b7b23b9789b093ef050fed488a
Danish Saeed <danishsaeed2@gmail.com>
2017-06-04 23:37:52 +0530

    Update description

--- 4_sem/10_radixsort.cpp
+++ 4_sem/10_radixsort.cpp
@@ -1,3 +1,5 @@
+// Implement radix sort on array
+
 #include<iostream>
 #include<cstdlib>
 #include<ctime>
--- 4_sem/8_mergesort.cpp
+++ 4_sem/8_mergesort.cpp
@@ -1,3 +1,5 @@
+// Implement merge sort on array
+
 #include<iostream>
 #include<cstdlib>
 #include<ctime>
